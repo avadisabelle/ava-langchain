fb6a315672e534c66621e8d1e754c6190fe45802
JGWill <jgi@jgwill.com>
2025-11-18 07:03:03 -0500

    potential local launch script

---
+++ LAUNCH__session_id__avaLangChainComponents_2511180702.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+. _env.sh
+export SESSION_ID=74f9e759-66ad-4f10-bebe-331f75f0742a
+export session_id__avaLangChainComponents_2511180702
+export session_id__avaLangChainComponents_2511180702__MCP_CONFIG
+export session_id__avaLangChainComponents_2511180702__ADD_DIR
+claude "see @GEMINI.md for intent and persona.  might want to look also in /src/holisticagnostic-narrative-context-protocol/ to see if because of the NCP, we need special components" --mcp-config $session_id__avaLangChainComponents_2511180702__MCP_CONFIG --add-dir $session_id__avaLangChainComponents_2511180702__ADD_DIR --session-id $session_id__avaLangChainComponents_2511180702
--- _env.sh
+++ _env.sh
@@ -1,3 +1,10 @@
 session_id__avaAgenticFlywheelFlowiseLangflowCommonTracingSessionID_2511180358=a50f3fc2-eb8c-434d-a37e-ef9615d9c07d
 
 session_id__avaLangChain_2511180604=e0ed5d45-d62a-47d3-b757-e788713e2106
+  
+# Namespace: avaLangChainComponents 
+session_id__avaLangChainComponents_2511180702=74f9e759-66ad-4f10-bebe-331f75f0742a
+alias cdavaLangChainComponents="cd /src/_sessiondata/74f9e759-66ad-4f10-bebe-331f75f0742a && ls -ltra" 
+session_id__avaLangChainComponents_2511180702__MCP_CONFIG="/src/.mcp.coaiapy.env.aetherial.json"
+session_id__avaLangChainComponents_2511180702__ADD_DIR="/src/IAIP /src/llms /src/_sessiondata/ /src/holisticagnostic-narrative-context-protocol/"
+  
